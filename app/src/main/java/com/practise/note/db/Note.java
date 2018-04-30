b0ac7690368c4d7338b2d9d134224775783a43d6
pepper-code <18716023691@163.com>
2018-04-30 16:47:43 +0800

    加入创建数据库表和配置

---
+++ app/src/main/java/com/practise/note/db/Note.java
@@ -0,0 +1,77 @@
+package com.practise.note.db;
+
+import org.litepal.crud.DataSupport;
+
+import java.util.Date;
+
+/**
+ * Author: Pepper
+ * Date :  2018/4/30
+ * Summary:${实体}.
+ */
+
+public class Note extends DataSupport {
+    private int notesId;
+    private String noteName;
+    private Date createTime;
+    private Boolean isDelete;
+    private Date modifyTime;
+    private int bookId;
+    private String noteContent;
+
+    public int getNotesId() {
+        return notesId;
+    }
+
+    public void setNotesId(int notesId) {
+        this.notesId = notesId;
+    }
+
+    public String getNoteName() {
+        return noteName;
+    }
+
+    public void setNoteName(String noteName) {
+        this.noteName = noteName;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Boolean getIsDelete() {
+        return isDelete;
+    }
+
+    public void setIsDelete(Boolean isDelete) {
+        this.isDelete = isDelete;
+    }
+
+    public Date getModifyTime() {
+        return modifyTime;
+    }
+
+    public void setModifyTime(Date modifyTime) {
+        this.modifyTime = modifyTime;
+    }
+
+    public int getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(int bookId) {
+        this.bookId = bookId;
+    }
+
+    public String getNoteContent() {
+        return noteContent;
+    }
+
+    public void setNoteContent(String noteContent) {
+        this.noteContent = noteContent;
+    }
+}
