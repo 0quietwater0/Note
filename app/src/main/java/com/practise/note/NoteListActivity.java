04cf8e767eb4b99e6cd37749678b8eb55fe6b4fd
pepper-code <18716023691@163.com>
2018-04-30 23:45:00 +0800

    Signed-off-by: pepper-code <18716023691@163.com>

--- app/src/main/java/com/practise/note/NoteListActivity.java
+++ app/src/main/java/com/practise/note/NoteListActivity.java
@@ -2,12 +2,16 @@ package com.practise.note;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class NoteListActivity extends AppCompatActivity {
-
+    Toolbar mToolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note_list);
+        Toolbar mToolbar =  findViewById(R.id.toolbar);
+        mToolbar.setTitle("");
+        setSupportActionBar(mToolbar);
     }
 }
